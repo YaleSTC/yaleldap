c8ab5c19221a06467806f349e1a0b809dbc4c974
caseywatts <casey.s.watts@gmail.com>
2014-09-21 14:02:11 -0400

    trying method_missing to reduce redundancy, not sure how I feel about it

--- lib/yaleldap.rb
+++ lib/yaleldap.rb
@@ -15,67 +15,118 @@ module YaleLDAP
   # The most common Yale LDAP atttributes that we care about extracting
   LDAP_ATTRS = %w(uid givenname sn mail collegename college class UPI)
 
+  # ##
+  # # Lookup LDAP information by upi
+  # # 
+  # # @param
+  # #   upi as a string, ex "12714662"
+  # #
+  # # @return
+  # #   Standard hash (see extract_attributes)
+  # # 
+  # # @example
+  # #   YaleLDAP.lookup_by_upi("12714662")
+
+  # def self.lookup_by_upi(upi)
+  #   ldap = Net::LDAP.new host: LDAP_HOST, port: LDAP_PORT
+  #   upifilter = Net::LDAP::Filter.eq('UPI', upi)
+  #   ldap_response = ldap.search(base: LDAP_BASE,
+  #                    filter: upifilter,
+  #                    attributes: LDAP_ATTRS)
+  #   extract_attributes(ldap_response)
+  # end
+
+  # ##
+  # # Lookup LDAP information by netid
+  # # 
+  # # @param
+  # #   netid as a string, ex "csw3"
+  # #
+  # # @return
+  # #   Standard hash (see extract_attributes)
+  # # 
+  # # @example
+  # #   YaleLDAP.lookup_by_netid("csw3")
+  # #
+  # def self.lookup_by_netid(netid)
+  #   ldap = Net::LDAP.new host: LDAP_HOST, port: LDAP_PORT
+  #   upifilter = Net::LDAP::Filter.eq('uid', netid)
+  #   ldap_response = ldap.search(base: LDAP_BASE,
+  #                    filter: upifilter,
+  #                    attributes: LDAP_ATTRS)
+  #   extract_attributes(ldap_response)
+  # end
+
+  # ##
+  # # Lookup LDAP information by Yale email address
+  # # 
+  # # @param
+  # #   email as a string, ex "casey.watts@yale.edu"
+  # #
+  # # @return
+  # #   Standard hash (see extract_attributes)
+  # # 
+  # # @example
+  # #   YaleLDAP.lookup_by_email("casey.watts@yale.edu")
+  # #
+  # def self.lookup_by_email(email)
+  #   lookup_type = 'mail'
+  #   lookup_query = email
+  #   ldap = Net::LDAP.new host: LDAP_HOST, port: LDAP_PORT
+  #   upifilter = Net::LDAP::Filter.eq(lookup_type, lookup_query)
+  #   ldap_response = ldap.search(base: LDAP_BASE,
+  #                    filter: upifilter,
+  #                    attributes: LDAP_ATTRS)
+  #   extract_attributes(ldap_response)
+  # end
+
   ##
-  # Lookup LDAP information by upi
-  # 
+  # lookup by an arbitrary query (netid, email, upi, etc)
+  #
   # @param
-  #   upi as a string, ex "12714662"
+  #   a hash with the format {:querytype => specificquery}
+  #   querytype accepts 'mail', 'uid', 'UPI'
   #
   # @return
-  #   Standard hash (see extract_attributes)
-  # 
+  #   standard hash (see extract_attributes)
+  #
   # @example
-  #   YaleLDAP.lookup_by_upi("12714662")
+  #   YaleLDAP.lookup(mail: "casey.watts@yale.edu")
+  #   YaleLDAP.lookup(uid: "csw3")
+  #   YaleLDAP.lookup(UPI: "12714662")
+  #
+  def lookup(lookup_type, lookup_query)
+    # construct query
+    # hardcoded for now
+    # lookup_type = 'mail'
+    # lookup_query = 'casey.watts@yale.edu'
+    lookup_filter = Net::LDAP::Filter.eq(lookup_type, lookup_query)
 
-  def self.lookup_by_upi(upi)
+    # execute query
     ldap = Net::LDAP.new host: LDAP_HOST, port: LDAP_PORT
-    upifilter = Net::LDAP::Filter.eq('UPI', upi)
     ldap_response = ldap.search(base: LDAP_BASE,
-                     filter: upifilter,
+                     filter: lookup_filter,
                      attributes: LDAP_ATTRS)
     extract_attributes(ldap_response)
   end
 
   ##
-  # Lookup LDAP information by netid
-  # 
-  # @param
-  #   netid as a string, ex "csw3"
+  # auto-generates many lookup methods for us.
   #
-  # @return
-  #   Standard hash (see extract_attributes)
-  # 
-  # @example
-  #   YaleLDAP.lookup_by_netid("csw3")
-  #
-  def self.lookup_by_netid(netid)
-    ldap = Net::LDAP.new host: LDAP_HOST, port: LDAP_PORT
-    upifilter = Net::LDAP::Filter.eq('uid', netid)
-    ldap_response = ldap.search(base: LDAP_BASE,
-                     filter: upifilter,
-                     attributes: LDAP_ATTRS)
-    extract_attributes(ldap_response)
-  end
-
-  ##
-  # Lookup LDAP information by Yale email address
-  # 
-  # @param
-  #   email as a string, ex "casey.watts@yale.edu"
-  #
-  # @return
-  #   Standard hash (see extract_attributes)
-  # 
   # @example
   #   YaleLDAP.lookup_by_email("casey.watts@yale.edu")
+  #     => YaleLDAP.lookup("email", "casey.watts@yale.edu")
   #
-  def self.lookup_by_email(email)
-    ldap = Net::LDAP.new host: LDAP_HOST, port: LDAP_PORT
-    upifilter = Net::LDAP::Filter.eq('mail', email)
-    ldap_response = ldap.search(base: LDAP_BASE,
-                     filter: upifilter,
-                     attributes: LDAP_ATTRS)
-    extract_attributes(ldap_response)
+  def method_missing(method_name, *arguments, &block)
+    if method_name.to_s =~ /lookup_by_(.*)/
+      lookup.send($1, *arguments, &block)
+    else
+      super
+    end
+  end
+
+  def respond_to?(method_name, include_private = false)
+    method_name.to_s.start_with?('lookup_by_') || super
   end
 
 private
